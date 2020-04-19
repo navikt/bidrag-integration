6818979932b551f878422733578d9f9f49333366
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2020-04-19 16:00:31 +0200

    new build of action with set -x in script

--- move/index.js
+++ move/index.js
@@ -4,11 +4,11 @@ const exec = require("@actions/exec");
 async function run() {
   try {
     const folderMoveFrom = core.getInput('folder_move_from');
-    const folderCopy = core.getInput('folder_copy');
+    const folderForHtml = core.getInput('folder_for_html');
 
     // Execute tag bash script
     await exec.exec(
-        `bash ${__dirname}/../move.sh "${folderMoveFrom}" "${folderCopy}"`
+        `bash ${__dirname}/../move.sh "${folderMoveFrom}" "${folderForHtml}"`
     );
 
   } catch (error) {
--- move/move.sh
+++ move/move.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -e
+set -x
 
 ############################################
 #
 # Følgende skjer i dette skriptet:
-# 1) setter input fra script (mappe hvor innhold skal kopieres og flyttes fra, samt navn på generert mappe for html)
+# 1) setter input fra script (mappe hvor innhold skal kopieres og flyttes fra, samt navn på mappa som skal inneholde generert html)
 # 2) oppretter generert mappe
 # 3) kopierer generert html til generert mappe
 # 4) flytter generert html til github pages
@@ -12,16 +12,16 @@ set -e
 ############################################
 
 INPUT_FOLDER_MOVE_FROM=$1
-INPUT_FOLDER_GENERATED=$2
+INPUT_FOLDER_FOR_HTML=$2
 
 if [[ $# -ne 2 ]]; then
-  echo "Usage: move.sh [target/html/folder] [name-of-generated-folder]"
+  echo "Usage: move.sh [target/html/folder] [name-of-folder-for-html]"
   exit 1;
 fi
 
-echo "Flytter html fra mappe $PWD/$INPUT_FOLDER_MOVE_FROM til mappe $PWD/docs samt oppretter en kopi i $PWD/generated/$INPUT_FOLDER_GENERATED."
+echo "Flytter html fra mappe $PWD/$INPUT_FOLDER_MOVE_FROM til mappe $PWD/docs samt oppretter en kopi i $PWD/generated/$INPUT_FOLDER_FOR_HTML."
 
-mkdir $PWD/generated/${INPUT_FOLDER_GENERATED}
-cp -r $PWD/${INPUT_FOLDER_MOVE_FROM}/* $PWD/generated/${INPUT_FOLDER_GENERATED}/.
+mkdir $PWD/generated/${INPUT_FOLDER_FOR_HTML}
+cp -r $PWD/${INPUT_FOLDER_MOVE_FROM}/* $PWD/generated/${INPUT_FOLDER_FOR_HTML}/.
 rm -rf $PWD/docs/*
 sudo mv $PWD/${INPUT_FOLDER_MOVE_FROM}/* $PWD/docs/.
