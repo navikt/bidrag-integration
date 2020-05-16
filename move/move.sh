7ad6d110d70d459d308464d4df9db5f45056d9c0
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2020-05-16 14:24:53 +0200

    the latest generated report exists in docs/latest/*

--- move/move.sh
+++ move/move.sh
@@ -7,15 +7,15 @@ set -e
 # 1) setter input fra script (mappe hvor innhold skal kopieres og flyttes fra)
 # 2) oppretter generert mappe under docs/generated
 # 3) kopierer generert html til generert mappe
-# 4) sletter gamle genererte html mapper rett under docs mappa
-# 5) flytter (og overskriver gammel) generert html til github pages (docs mappa)
+# 4) sletter gamle genererte html mapper rett under docs/latest mappa
+# 5) flytter (og overskriver gammel) generert html til github pages (docs/latest mappa)
 #
 ############################################
 
 INPUT_FOLDER_MOVE_FROM=$1
 
 if [[ $# -ne 1 ]]; then
-  echo "Usage: move.sh [relative/path/to/html/folder/to/move]"
+  echo "Usage: report.sh [relative/path/to/html/folder/to/move]"
   exit 1;
 fi
 
@@ -32,10 +32,10 @@ if [[ -d "$PROJECT_ROOT/docs/generated/$GENERATED_FOLDER" ]]; then
   GENERATED_FOLDER=$(date +"%Y-%m-%d0.%T")
 fi
 
-echo "Flytter html fra mappe $PROJECT_ROOT/$INPUT_FOLDER_MOVE_FROM til mappe $PROJECT_ROOT/docs"
+echo "Flytter html fra mappe $PROJECT_ROOT/$INPUT_FOLDER_MOVE_FROM til mappe $PROJECT_ROOT/docs/latest"
 echo "Oppretter også en kopi i $PROJECT_ROOT/docs/generated/$GENERATED_FOLDER"
 
 mkdir ${PROJECT_ROOT}/docs/generated/${GENERATED_FOLDER}
 cp -R ${PROJECT_ROOT}/${INPUT_FOLDER_MOVE_FROM}/* ${PROJECT_ROOT}/docs/generated/${GENERATED_FOLDER}/.
-cd ${PROJECT_ROOT}/docs && ls | grep -v generated | xargs rm -rf
-sudo mv ../${INPUT_FOLDER_MOVE_FROM}/* .
+cd ${PROJECT_ROOT}/docs/latest && ls |  xargs rm -rf
+sudo mv ${PROJECT_ROOT}/${INPUT_FOLDER_MOVE_FROM}/* .
