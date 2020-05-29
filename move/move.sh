b1a750ea50c9bcd38abd5872c5ca0c0c67c4e916
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-05-29 12:24:15 +0200

    project_where_to_move and full paths instead of $PWD

--- move/move.sh
+++ move/move.sh
@@ -9,8 +9,8 @@ set -x
 # - alle genererte rapporter ligger under mappa docs/generated/<date or timestamp>
 #
 # Følgende skjer i dette skriptet:
-# 1) setter input fra script (mappe hvor innhold skal kopieres og flyttes fra)
-# 2) oppretter generert mappe under docs/generated
+# 1) setter miljørvariabler
+# 2) oppretter generert mappe under docs/generated (oppretter timestampted mappe hvis dato-mappe finnes)
 # 3) kopierer generert html til generert mappe
 # 4) sletter gamle genererte html mapper rett under docs/latest mappa
 # 5) flytter (og overskriver gammel) generert html til github pages (docs/latest mappa)
@@ -18,35 +18,38 @@ set -x
 ############################################
 
 if [[ $# -ne 1 ]]; then
-  echo "Usage: report.sh [relative/path/to/html/folder/for/generated/html]"
+  echo "Usage: report.sh [path/to/html/folder/for/generated/html]"
   exit 1;
 fi
 
 INPUT_FOLDER_MOVE_FROM=$1
-PROJECT_ROOT="$PWD"
-GH_PAGES_GENERATED="$PROJECT_ROOT/docs/generated"
+INPUT_PROJECT_WHERE_TO_MOVE=$2
 
-if [[ ! -d "$PROJECT_ROOT/$INPUT_FOLDER_MOVE_FROM" ]]; then
-  echo ::error:: "unable to locate folder to move from $PROJECT_ROOT/$INPUT_FOLDER_MOVE_FROM"
+FULL_PATH_TO_MOVE_FOLDER="$PWD/$(find . -d | grep "$INPUT_FOLDER_MOVE_FROM")"
+FULL_PATH_TO_ROOT_PROJECT="$PWD/$(find . -type f | grep "$INPUT_PROJECT_WHERE_TO_MOVE/README.md" | sed 's;/README.md;;')"
+FULL_PATH_TO_DOCS_LATEST="$FULL_PATH_TO_ROOT_PROJECT/docs/latest"
+FULL_PATH_TO_DOCS_GENERATED="$FULL_PATH_TO_ROOT_PROJECT/docs/generated"
+
+if [[ ! -d "$FULL_PATH_TO_ROOT_PROJECT/$INPUT_FOLDER_MOVE_FROM" ]]; then
+  echo ::error:: "unable to locate folder to move from - $INPUT_FOLDER_MOVE_FROM (using full path: $FULL_PATH_TO_MOVE_FOLDER)"
   exit 1;
 fi
 
-echo "Move generated html from $PROJECT_ROOT/$INPUT_FOLDER_MOVE_FROM"
-
 GENERATED_FOLDER=$(date +"%Y-%m-%d")
 
-if [[ -d "$GH_PAGES_GENERATED/$GENERATED_FOLDER" ]]; then
+echo "Move generated html from $FULL_PATH_TO_MOVE_FOLDER"
+
+if [[ -d "$FULL_PATH_TO_DOCS_GENERATED/$GENERATED_FOLDER" ]]; then
   GENERATED_FOLDER=$(date +"%Y-%m-%d.%T")
 fi
 
-mkdir $GENERATED_FOLDER
+FULL_PATH_TO_GENERATED_FOLDER="$FULL_PATH_TO_DOCS_GENERATED/$GENERATED_FOLDER"
+mkdir "$FULL_PATH_TO_GENERATED_FOLDER"
 
-GH_PAGES_LATEST="$PROJECT_ROOT/docs/latest"
+echo "Flytter html fra mappe $FULL_PATH_TO_MOVE_FOLDER til mappe $FULL_PATH_TO_DOCS_LATEST"
+echo "Oppretter også en kopi i $FULL_PATH_TO_GENERATED_FOLDER"
 
-echo "Flytter html fra mappe $PROJECT_ROOT/$INPUT_FOLDER_MOVE_FROM til mappe $GH_PAGES_LATEST"
-echo "Oppretter også en kopi i $PROJECT_ROOT/docs/generated/$GENERATED_FOLDER"
-
-mkdir ${PROJECT_ROOT}/docs/generated/${GENERATED_FOLDER}
-cp -R ${PROJECT_ROOT}/${INPUT_FOLDER_MOVE_FROM}/* ${PROJECT_ROOT}/docs/generated/${GENERATED_FOLDER}/.
-cd ${PROJECT_ROOT}/docs/latest && ls | xargs rm -rf
-sudo mv ${PROJECT_ROOT}/${INPUT_FOLDER_MOVE_FROM}/* .
+mkdir ${FULL_PATH_TO_GENERATED_FOLDER}
+cp -R ${FULL_PATH_TO_MOVE_FOLDER}/* ${FULL_PATH_TO_GENERATED_FOLDER}/.
+cd ${FULL_PATH_TO_DOCS_LATEST} && ls | xargs rm -rf
+sudo mv ${FULL_PATH_TO_MOVE_FOLDER}/* .
