7e0b2ed273fe440923cd5f66aaeedca077840dcb
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-05-30 20:18:33 +0000

    2020-05-30:20:18:33: Build of actions with npm and ncc

--- reports/dist/index.js
+++ reports/dist/index.js
@@ -1501,9 +1501,12 @@ const exec = __webpack_require__(960);
 async function run() {
   try {
     const pagesAddress = core.getInput('pages_address');
+    const projectWhereToMove = core.getInput('project_where_to_move');
 
     // Execute tag bash script
-    await exec.exec(`bash ${__dirname}/../report.sh "${pagesAddress}"`);
+    await exec.exec(
+        `bash ${__dirname}/../report.sh "${pagesAddress}" "${projectWhereToMove}"`
+    );
 
   } catch (error) {
     core.setFailed(error.message);
