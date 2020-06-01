0cc0d7193301e1a53bb99192264e5a723c37b2d9
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-06-01 14:02:18 +0000

    2020-06-01:14:02:18: Build of actions with npm and ncc

--- move/dist/index.js
+++ move/dist/index.js
@@ -1502,10 +1502,11 @@ async function run() {
   try {
     const folderMoveFrom = core.getInput('folder_move_from');
     const projectWhereToMove = core.getInput('project_where_to_move');
+    const frontPage = core.getInput('front_page');
 
     // Execute tag bash script
     await exec.exec(
-        `bash ${__dirname}/../move.sh "${folderMoveFrom}" "${projectWhereToMove}"`
+        `bash ${__dirname}/../move.sh "${folderMoveFrom}" "${projectWhereToMove}" "${frontPage}"`
     );
 
   } catch (error) {
