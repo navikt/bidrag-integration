89bbd73ad5ee8aa161c23f638ac663520ef8275d
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-08-03 19:03:13 +0200

    always clone project to get the nais configuration

--- cucumber-clone/clone-cucumber.sh
+++ cucumber-clone/clone-cucumber.sh
@@ -4,18 +4,15 @@ set -x
 ############################################
 #
 # Følgende skjer i dette skriptet:
-# 1) setter input
-# 2) sletter bidrag-cucumber-backend hvis den finnes fra før
-# 3a) ved feature branch
+# 1) sletter bidrag-cucumber-backend hvis den finnes fra før
+# 2a) ved feature branch
 #    - clone bidrag-cucumber-backend, branch=feature (hvis den finnes), hvis ikke brukes master
-# 3b) ved master branch
+# 2b) ved master branch
 #    - clone bidrag-cucumber-backend master
-# 4) sjekker om vi har all konfigurasjon som trengs til integrasjonstestingen (passord for nav-bruker og testbrukere)
-# 5) når NAIS_GITHUB_PROJECT er angitt, lages mappa simple hvor github prosjektet klones til
+# 3) sjekker om vi har all konfigurasjon som trengs til integrasjonstestingen (passord for nav-bruker og testbrukere)
+# 4) klon GITHUB_REPOSITORY i mappa "simple" for at cucumber skal lese nais-konfigurasjon
 #
 ############################################
-
-INPUT_NAIS_GITHUB_PROJECT=$1
 
 sudo rm -rf bidrag-cucumber-backend
 BRANCH="${GITHUB_REF#refs/heads/}"
@@ -38,29 +35,25 @@ else
   git clone "--depth 1 https://github.com/navikt/bidrag-cucumber-backend"
 fi
 
-if [ -n "${INPUT_NAIS_GITHUB_PROJECT}" ]
-then
+# gå til bidrag-cucumber-backend slik at json filene blir synlige i docker container når integrasjonstestene gjøres
+cd bidrag-cucumber-backend || exit 1;
 
-  # gå til bidrag-cucumber-backend slik at json filene blir synlige i docker container når integrasjonstestene gjøres
-  cd bidrag-cucumber-backend || exit 1;
-
-  if [[ "$BRANCH" == "master" ]]; then
-    CLONE_BRANCH=""
-  else
-    CLONE_BRANCH="--branch=$BRANCH"
-  fi
-
-  SIMPLE="$PWD/simple"
-
-  sudo rm -rf "$SIMPLE"
-  mkdir "$SIMPLE"
-  cd "$SIMPLE" || exit 1;
-
-  if [[ -z "$GITHUB_TOKEN" ]]; then
-    git clone "--depth 1 $CLONE_BRANCH https://github.com/navikt/$INPUT_NAIS_GITHUB_PROJECT"
-  else
-    git clone "--depth 1 $CLONE_BRANCH https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/navikt/$INPUT_NAIS_GITHUB_PROJECT"
-  fi
-
-  find . -type f -name "q*.json"
+if [[ "$BRANCH" == "master" ]]; then
+  CLONE_BRANCH=""
+else
+  CLONE_BRANCH="--branch=$BRANCH"
 fi
+
+SIMPLE="$PWD/simple"
+
+sudo rm -rf "$SIMPLE"
+mkdir "$SIMPLE"
+cd "$SIMPLE" || exit 1;
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  git clone "--depth 1 $CLONE_BRANCH https://github.com/$GITHUB_REPOSITORY"
+else
+  git clone "--depth 1 $CLONE_BRANCH https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
+fi
+
+find . -type f -name "q*.json"
--- cucumber-clone/index.js
+++ cucumber-clone/index.js
@@ -3,10 +3,8 @@ const exec = require("@actions/exec");
 
 async function run() {
   try {
-    const naisGithubProject = core.getInput('nais_github_project');
-
     // Execute cucumber-clone bash script
-    await exec.exec(`${__dirname}/../cucumber-clone.sh`, [naisGithubProject]);
+    await exec.exec(`${__dirname}/../cucumber-clone.sh`);
   } catch (error) {
     core.setFailed(error.message);
   }
