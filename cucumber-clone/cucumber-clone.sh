812a2ff2786511376de6a5dba9c2523c094c6e97
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-08-05 16:23:18 +0200

    do not wrap with double quotes and ignore warnings

--- cucumber-clone/cucumber-clone.sh
+++ cucumber-clone/cucumber-clone.sh
@@ -16,29 +16,31 @@ set -x
 
 INPUT_CUCUMBER_PROJECT=$1
 
-sudo rm -rf "INPUT_CUCUMBER_PROJECT"
+sudo rm -rf "$INPUT_CUCUMBER_PROJECT"
 BRANCH="${GITHUB_REF#refs/heads/}"
 
 if [[ "$GITHUB_REF" != "refs/heads/master" ]]; then
   FEATURE_BRANCH=$BRANCH
   # shellcheck disable=SC2046
-  IS_API_CHANGE="$(git ls-remote "--heads $(echo "https://github.com/navikt/$INPUT_CUCUMBER_PROJECT $FEATURE_BRANCH" | sed "s/'//g")" | wc -l)"
+  IS_API_CHANGE=$(git ls-remote --heads $(echo "https://github.com/navikt/$INPUT_CUCUMBER_PROJECT $FEATURE_BRANCH" | sed "s/'//g") | wc -l)
 
   if [[ $IS_API_CHANGE -eq 1 ]]; then
     echo "Using feature branch: $FEATURE_BRANCH"
     # shellcheck disable=SC2086
-    git clone "$("--depth 1 --branch=$FEATURE_BRANCH https://github.com/navikt/$INPUT_CUCUMBER_PROJECT" | sed "s/'//g")"
+    git clone --depth 1 --branch=$FEATURE_BRANCH https://github.com/$INPUT_CUCUMBER_PROJECT
   else
     echo "Using /refs/heads/master"
-    git clone "$("--depth 1 https://github.com/navikt/$INPUT_CUCUMBER_PROJECT" | sed "s/'//g")"
+    # shellcheck disable=SC2086
+    git clone --depth 1 https://github.com/navikt/$INPUT_CUCUMBER_PROJECT
   fi
 else
   echo "Using /refs/heads/master"
-  git clone "$(echo "--depth 1 https://github.com/navikt/$INPUT_CUCUMBER_PROJECT" | sed "s/'//g")"
+  # shellcheck disable=SC2086
+  git clone --depth 1 https://github.com/navikt/$INPUT_CUCUMBER_PROJECT
 fi
 
 # gå til "INPUT_CUCUMBER_PROJECT" slik at json filene blir synlige i docker container når integrasjonstestene kjøres
-cd "INPUT_CUCUMBER_PROJECT" || exit 1;
+cd "$INPUT_CUCUMBER_PROJECT" || exit 1;
 
 if [[ "$BRANCH" == "master" ]]; then
   CLONE_BRANCH=""
@@ -50,9 +52,11 @@ mkdir simple
 cd simple || exit 1;
 
 if [[ -z "$GITHUB_TOKEN" ]]; then
-  git clone "--depth 1 $CLONE_BRANCH https://github.com/$GITHUB_REPOSITORY"
+  # shellcheck disable=SC2086
+  git clone --depth 1 $CLONE_BRANCH https://github.com/$GITHUB_REPOSITORY
 else
-  git clone "--depth 1 $CLONE_BRANCH https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
+  # shellcheck disable=SC2086
+  git clone --depth 1 $CLONE_BRANCH https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY
 fi
 
 find . -type f -name "q*.json"
