911f95d97d6f8fc84d9c6149738f50602cc59303
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-08-05 11:06:43 +0200

    created cucumber-clone acrion

---
+++ cucumber-clone/clone-cucumber.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -x
+
+############################################
+#
+# Følgende skjer i dette skriptet:
+# 1) setter cucumber prosjekt som input og sletter cucumber prosjektet hvis den finnes fra før
+# 2a) ved feature branch
+#    - clone cucumber-prosjektet, branch=feature (hvis den finnes), hvis ikke brukes master
+# 2b) ved master branch
+#    - clone cucumber-prosjektet, master branch
+# 3) sjekker om vi har all konfigurasjon som trengs til integrasjonstestingen (passord for nav-bruker og testbrukere)
+# 4) klon GITHUB_REPOSITORY i mappa "simple" for at cucumber skal lese nais-konfigurasjon
+#
+############################################
+
+INPUT_CUCUMBER_PROJECT=$1
+
+sudo rm -rf "INPUT_CUCUMBER_PROJECT"
+BRANCH="${GITHUB_REF#refs/heads/}"
+
+if [[ "$GITHUB_REF" != "refs/heads/master" ]]; then
+  FEATURE_BRANCH=$BRANCH
+  # shellcheck disable=SC2046
+  IS_API_CHANGE="$(git ls-remote --heads ""$(echo "https://github.com/navikt/$INPUT_CUCUMBER_PROJECT $FEATURE_BRANCH" | sed "s/'//g") | wc -l)"
+
+  if [[ $IS_API_CHANGE -eq 1 ]]; then
+    echo "Using feature branch: $FEATURE_BRANCH"
+    # shellcheck disable=SC2086
+    git clone "--depth 1 --branch=$FEATURE_BRANCH https://github.com/navikt/$INPUT_CUCUMBER_PROJECT"
+  else
+    echo "Using /refs/heads/master"
+    git clone "--depth 1 https://github.com/navikt/$INPUT_CUCUMBER_PROJECT"
+  fi
+else
+  echo "Using /refs/heads/master"
+  git clone "--depth 1 https://github.com/navikt/$INPUT_CUCUMBER_PROJECT"
+fi
+
+# gå til "INPUT_CUCUMBER_PROJECT" slik at json filene blir synlige i docker container når integrasjonstestene gjøres
+cd "INPUT_CUCUMBER_PROJECT" || exit 1;
+
+if [[ "$BRANCH" == "master" ]]; then
+  CLONE_BRANCH=""
+else
+  CLONE_BRANCH="--branch=$BRANCH"
+fi
+
+SIMPLE="$PWD/simple"
+
+sudo rm -rf "$SIMPLE"
+mkdir "$SIMPLE"
+cd "$SIMPLE" || exit 1;
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  git clone "--depth 1 $CLONE_BRANCH https://github.com/$GITHUB_REPOSITORY"
+else
+  git clone "--depth 1 $CLONE_BRANCH https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
+fi
+
+find . -type f -name "q*.json"
