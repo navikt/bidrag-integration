5c95fddc482e481557c7efbee165e9ca12aea80b
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-08-28 15:56:15 +0200

    providing credentials as environment variable for cloning of repositories outside workflow
    - remove command substitution

--- cucumber-clone/cucumber-clone.sh
+++ cucumber-clone/cucumber-clone.sh
@@ -4,7 +4,8 @@ set -x
 ############################################
 #
 # Følgende skjer i dette skriptet:
-# 1) sjekker antall og setter inputs og sletter cucumber prosjektet hvis det finnes fra før
+# 1a) sjekker antall og setter inputs og sletter cucumber prosjektet hvis det finnes fra før
+# 1b) sjekker at miljøvariabelen EXTRA_CLONES_CREDENTIALS er oppgitt
 # 2a) ved feature branch
 #    - clone cucumber-prosjektet, branch=feature (hvis den finnes), hvis ikke brukes main
 # 2b) ved main branch
@@ -16,14 +17,21 @@ set -x
 ############################################
 
 if [[ $# -ne 4 ]]; then
-  echo "Bruk: cucumber-clone.sh <cucumber_prosjekt> <folder_nais_apps> [<extra clones>] <delimiter>, see action.yaml (third argument is optional)"
+  echo "Bruk: cucumber-clone.sh <cucumber_prosjekt> <folder_nais_apps> <delimiter> [<extra clones>], see action.yaml (last arguments are optional)"
   exit 1;
 fi
 
 INPUT_CUCUMBER_PROJECT=$1
 INPUT_FOLDER_NAIS_APPS=$2
-INPUT_EXTRA_CLONES=$3
-INPUT_DELIMITER=$4
+INPUT_DELIMITER=$3
+INPUT_EXTRA_CLONES=$4
+
+if [[ -n $INPUT_EXTRA_CLONES ]]; then
+  if [[ -z $EXTRA_CLONES_CREDENTIALS ]]; then
+    echo ::error:: "Credentials to clone external repository must be provided, value: <github username>:<github personal access token>"
+    exit 1
+  fi
+fi
 
 cd "$RUNNER_WORKSPACE" || exit 1 # cd til RUNNER_WORKSPACE eller hard exit
 sudo rm -rf "$INPUT_CUCUMBER_PROJECT"
@@ -69,19 +77,16 @@ fi
 
 # shellcheck disable=SC2046
 # shellcheck disable=SC2086
-# git clone $(echo -v --depth 1 $CLONE_BRANCH https://"$CREDENTIALS"github.com/$GITHUB_REPOSITORY)
+git clone --depth 1 ${CLONE_BRANCH} https://${CREDENTIALS}github.com/${GITHUB_REPOSITORY}
 
 if [[ -n $INPUT_EXTRA_CLONES ]]; then
-#  IFS=$INPUT_DELIMITER
-#  read -ra clones <<< "$INPUT_EXTRA_CLONES"
+  IFS=$INPUT_DELIMITER
+  read -ra clones <<< "$INPUT_EXTRA_CLONES"
 
-#  for clone in "${clones[@]}"; do
-    # shellcheck disable=SC2046
-    git clone -v --depth 1 ${CLONE_BRANCH} https://${CREDENTIALS}github.com/${GITHUB_REPOSITORY}
-#    git clone -v --depth 1 --branch=main https://${CREDENTIALS}github.com/navikt/${clone}
-    git clone --depth 1 --branch=main https://jactor-rises:${GITHUB_TOKEN}@github.com/navikt/bidrag-dokument-testdata
-#    git clone -v --depth 1 --branch=main https://${CREDENTIALS}github.com/navikt/bidrag-dokument-testdata
-#  done
+  for clone in "${clones[@]}"; do
+    # shellcheck disable=SC2086
+    git clone --depth 1 --branch=main https://${EXTRA_CLONES_CREDENTIALS}@github.com/navikt/${clone}
+  done
 fi
 
 pwd
--- cucumber-clone/index.js
+++ cucumber-clone/index.js
@@ -4,14 +4,14 @@ const exec = require("@actions/exec");
 async function run() {
   const cucumberProject = core.getInput('cucumber_project')
   const folderNaisApps = core.getInput('folder_nais_apps')
-  const extraClones = core.getInput('extra_clones')
   const delimiter = core.getInput('delimiter')
+  const extraClones = core.getInput('extra_clones')
 
   try {
     // Execute cucumber-clone bash script
     await exec.exec(
         `${__dirname}/../cucumber-clone.sh`,
-        [cucumberProject, folderNaisApps, extraClones, delimiter]
+        [cucumberProject, folderNaisApps, delimiter, extraClones]
     );
   } catch (error) {
     core.setFailed(error.message);
