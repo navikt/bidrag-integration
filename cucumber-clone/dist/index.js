892dd8757943a523f74e8b7676dc3a99e56c33bc
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-08-28 10:43:59 +0000

    2020-08-28:10:43:58: Build of actions with npm and ncc

--- cucumber-clone/dist/index.js
+++ cucumber-clone/dist/index.js
@@ -1541,12 +1541,14 @@ const exec = __webpack_require__(960);
 async function run() {
   const cucumberProject = core.getInput('cucumber_project')
   const folderNaisApps = core.getInput('folder_nais_apps')
+  const extraClones = core.getInput('extra_clones')
+  const delimiter = core.getInput('delimiter')
 
   try {
     // Execute cucumber-clone bash script
     await exec.exec(
         `${__dirname}/../cucumber-clone.sh`,
-        [cucumberProject, folderNaisApps]
+        [cucumberProject, folderNaisApps, extraClones, delimiter]
     );
   } catch (error) {
     core.setFailed(error.message);
