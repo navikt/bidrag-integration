5f85a6fec80f0519bb3d2c23746ad95c02c0e5f0
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-09-04 12:30:09 +0200

    navigate to bidrag-cucumber-backend folder before checkout of feature branch
    also
    - turn off loggings of commands (set -x -> set -e)

--- cucumber-clone-tags/cucumber-clone-tags.sh
+++ cucumber-clone-tags/cucumber-clone-tags.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-set -x
+set -e
 
 ############################################
 #
 # Følgende skjer i dette skriptet
 # - Sjekker at miljøvariabelen WORKFLOW_CREDENTIALS er satt
 # - Går til RUNNER_WORKSPACE. og kloner bidrag-cucumber-backend, (på branch som bygges hvis det er bidrag-cucumber-backen)
-# - Kloner HEAD til bidrag-cucumber-backend
+# - Kloner bidrag-cucumber-backend (bare HEAD når det ikke er bidrag-cucumber-backend som bygges)
 # - Kloner HEAD til bidragsprosjektene som er tagget i cucumber-features
 #
 ############################################
@@ -21,12 +21,13 @@ fi
 if [[ $GITHUB_REPOSITORY == "navikt/bidrag-cucumber-backend" ]]; then
   BRANCH="${GITHUB_REF#refs/heads/}"
   git clone https://github.com/navikt/bidrag-cucumber-backend
+  cd bidrag-cucumber-backend || exit 1
   git checkout ${BRANCH}
 else
   git clone --depth 1 https://github.com/navikt/bidrag-cucumber-backend
+  cd bidrag-cucumber-backend || exit 1
 fi
 
-cd bidrag-cucumber-backend || exit 1
 USE_NAIS_APPS="$(find . -type f -name "*.feature" | xargs cat | grep @bidrag- | grep -v @bidrag-cucumber | sort -u | sed 's/@//')"
 
 mkdir simple
